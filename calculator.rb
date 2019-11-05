1b489aaf502da126d0bedfa4e5fe389e815da7a0
David Cha <david.cha44@qmail.cuny.edu>
2019-11-05 18:48:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,3 +7,7 @@ second_number = 5
 sum = first_number + second_number
 
 difference = first_number - second_number
+
+multiplying = first_number * second_number
+
+division = first_number / second_number
